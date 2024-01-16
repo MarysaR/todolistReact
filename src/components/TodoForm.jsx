0217d261b8bb63eac5ad3e5830788e2c3690f8d8
Neycha <regentmarysapro@gmail.com>
2024-01-16 07:50:12 +0100

    Todolist and TodoForm components

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState} from 'react';
+
+
+function TodoForm(props) {
+    const [input, setInput] = useState('')
+
+    const handleChange = e=> {
+        setInput(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // props.onSubmit([
+        //     id, Math.floor(Math.random() * 5),
+        //     text, input
+        // ]);
+
+        setInput('');
+            
+    };
+
+  return (
+    <form className='todo-form' onSubmit={ handleSubmit }>
+        <input 
+            type="text" 
+            placeholder='Ajouter une tâche' 
+            value={input}
+            name='text'
+            className='todo-input' 
+            onChange={ handleChange }
+        />
+
+            <button 
+                className='todo-button'>
+                    Ajouter une tâche
+            </button>
+    </form>
+  )
+}
+
+export default TodoForm;
